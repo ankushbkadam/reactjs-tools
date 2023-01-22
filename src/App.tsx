4e4ee5f94f4c91fdcbaf75ac21b76e9a97f8ff6d
Ankush Kadam <ankushbkadam@gmail.com>
2023-01-22 23:29:32 +0530

    footer added, about us page styles changed a bit, app.css unwanted css removed

--- src/App.tsx
+++ src/App.tsx
@@ -6,17 +6,19 @@ import { Route, Routes } from 'react-router';
 import Navbar from './Components/Navbar/Navbar';
 import { ROUTES } from './Constants/routeConstants';
 import About from './Components/About'
+import Footer from './Components/Footer';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <div className='container'>
+      <div>
         <Routes>
           <Route path={ROUTES.HOME} element={<Home />} />
           <Route path={ROUTES.ABOUT} element={<About/>}/>
         </Routes>
       </div>
+      <Footer />
     </div>
   );
 }
---
+++ src/Components/Footer/index.tsx
@@ -0,0 +1,12 @@
+import { ROUTES } from "../../Constants/routeConstants";
+
+const Footer = () => {
+    return <footer className="bg-dark text-center text-white fixed-bottom">
+        <div className="text-center p-3" style={{backgroundColor: 'rgba(0, 0, 0, 0.2)'}}>
+            © 2023 Copyright &nbsp;
+            <a className="text-white" href={ROUTES.ABOUT}>Ankush and Akshay</a>
+        </div>
+    </footer>
+}
+
+export default Footer;
