2003fab74475ef13695170edb1763f5e7ddaf4bb
Akshay Yelvankar <48756935+akshayyelvankar@users.noreply.github.com>
2023-01-22 16:43:16 +0530

    Install Bootstarp, Routing,creating Navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,19 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Home from './Components/Home/Home-page';
+import { Route, Routes } from 'react-router';
+import Navbar from './Components/Navbar/Navbar';
+import { ROUTES } from './Constants/routeConstants';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <Routes>
+        <Route path={ROUTES.HOME} element={<Home />} />
+        <Route path={ROUTES.ABOUT} element={<div>About</div>} />
+      </Routes>
     </div>
   );
 }
---
+++ src/Components/Navbar/Navbar.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { Route, Routes } from 'react-router';
+import { Link, NavLink } from 'react-router-dom'
+import { ROUTES } from "../../Constants/routeConstants";
+const Navbar = () => {
+    return (
+        <div>
+            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                <div className="container-fluid">
+                    <a className="navbar-brand" href={ROUTES.HOME}>Navbar</a>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarNavDropdown">
+                        <ul className="navbar-nav">
+                            <li className="nav-item">
+                                <a className="nav-link active" aria-current="page" href={ROUTES.HOME} >Home</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href={ROUTES.ABOUT}>About</a>
+                            </li>
+
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </div>
+    )
+}
+export default Navbar;
