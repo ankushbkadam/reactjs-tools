443fc960f05f8014a44cf79c007ebc3864d09955
Akshay <akshayyelvankar@gmail.com>
2023-01-23 15:21:00 +0530

    Created  Header Section & adding slider and also Created Card-section

--- src/Components/Home/Home-page.tsx
+++ src/Components/Home/Home-page.tsx
@@ -1,8 +1,47 @@
-const Home=()=>{
-    return(
-       <div className="container ">
-          <h1>Hello</h1>
-       </div>
-    )
+const Home = () => {
+   return (
+      <div className="">
+
+         <header className="container-fluid p-0">
+            <div id="carouselExampleIndicators" className="carousel slide" data-bs-ride="carousel">
+               <div className="carousel-indicators">
+                  <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
+                  <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                  <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+               </div>
+               <div className="carousel-inner">
+                  <div className="carousel-item active">
+                     <img src="./Images/p1.jpg" className="d-block w-100   " alt="pic-1" img-fluid />
+                  </div>
+                  <div className="carousel-item">
+                     <img src="./Images/p2.jpg" className="d-block w-100 " alt="pic-2" img-fluid />
+                  </div>
+                  <div className="carousel-item">
+                     <img src="/Images/p3.png" className="d-block w-100 " alt="pic-3" img-fluid />
+                  </div>
+               </div>
+               <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+                  <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                  <span className="visually-hidden">Previous</span>
+               </button>
+               <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+                  <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                  <span className="visually-hidden">Next</span>
+               </button>
+            </div>
+         </header>
+       
+         <section className=" container card-section mt-5">
+            <h2>Card Menu</h2>
+            <div className="card w-100 ">
+               <div className="card-body p-5">
+                  <h5 className="card-title">Card title</h5>
+                  <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                  <a href="#" className="btn btn-primary">Go to Converter</a>
+               </div>
+            </div>
+        </section>
+     </div>
+   )
 }
 export default Home;
