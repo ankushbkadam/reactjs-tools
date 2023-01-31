f20248cc13265fe6d7cfb254d03f28a8e8876f11
Akshay Yelvankar <48756935+akshayyelvankar@users.noreply.github.com>
2023-01-31 20:23:56 +0530

    done

--- src/Components/Navbar/Navbar.tsx
+++ src/Components/Navbar/Navbar.tsx
@@ -5,7 +5,7 @@ import { ROUTES } from "../../Constants/routeConstants";
 const Navbar = () => {
     return (
         <div>
-            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
                 <div className="container-fluid">
                     <a className="navbar-brand" href={ROUTES.HOME}>Navbar</a>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
@@ -17,7 +17,7 @@ const Navbar = () => {
                                 <a className="nav-link active" aria-current="page" href={ROUTES.HOME} >Home</a>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link" href={ROUTES.ABOUT}>About</a>
+                                <a className="nav-link active" href={ROUTES.ABOUT}>About</a>
                             </li>
 
                         </ul>
