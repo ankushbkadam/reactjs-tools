ff5fddebd69ef0fd883ab4e3df5996d90e949d58
Ankush Kadam <ankushbkadam@gmail.com>
2023-02-05 13:44:18 +0530

    reusability

--- src/Components/PercentageCalculator/index.tsx
+++ src/Components/PercentageCalculator/index.tsx
@@ -1,11 +1,21 @@
 import Difference from './Difference';
 
+/**
+ * Percentage calculators array, add more apps on the go in a col structure
+ */
+const CALC_INFO = [
+   <Difference />,
+   <div>Work in progress</div>,
+]
+
 const PercentageCalculator = () => {
     return <div className="container">
         <div className="row">
-            <div className="col">
-                <Difference />
-            </div>
+            {CALC_INFO.map((component, index) =>  (
+                <div className="col-md-4" key={index}>
+                    {component}
+                </div> 
+            ))}
         </div>
   </div>
 }
